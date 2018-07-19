2213ae9b25cc86d0c6c940e8fe3dcbdb28032fa1
Abel Enrique Cruz <abelenriquecruz@gmail.com>
2018-07-19 00:57:45 +0000

    automatically backed up by learn

---
+++ Sandbox.js
@@ -0,0 +1,12 @@
+/* Declare the variable */
+var word
+
+/* Assign a value to the variable */
+word = 'bird'
+
+console.log(word) /* 'bird' */
+
+/* Assign another value to the variable */
+word = 'dog'
+
+console.log(word) /* 'dog' */
